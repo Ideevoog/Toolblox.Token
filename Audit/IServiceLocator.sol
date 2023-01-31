e275f3432d9504c49d272aa3ec62aad7c96723ff
sgraphics <silver.sepp@gmail.com>
2023-01-31 15:09:22 +0200

    Update IServiceLocator.sol

--- Audit/IServiceLocator.sol
+++ Audit/IServiceLocator.sol
@@ -4,5 +4,5 @@ pragma solidity >=0.7.0 <0.9.0;
 interface IServiceLocator {
     function getService(bytes32 name) external view returns (address);
     function getScheduler(address service) external view returns (address);
-    function registerService(bytes32 name, address destination) external;
+    function registerService(bytes32 name, bytes memory code) external returns (address);
 }
