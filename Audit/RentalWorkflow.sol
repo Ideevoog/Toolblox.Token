3a6658c91ce8b4ecc73c0c3525ca7bb0ab3e8534
sgraphics <silver.sepp@gmail.com>
2023-08-24 14:27:14 +0300

    Add comments

--- Audit/RentalWorkflow.sol
+++ Audit/RentalWorkflow.sol
@@ -3,6 +3,15 @@ pragma solidity ^0.8.0;
 import "../Contracts/WorkflowBase.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/*
+    A generic rental agreement contract.
+    Rental states:
+        3:Available
+        1:In use
+        2:Returned
+        4:Settled
+        5:Completed
+ */
 contract RentalWorkflow  is WorkflowBase, Ownable{
 	struct Rental {
 		uint id;
@@ -57,6 +66,38 @@ contract RentalWorkflow  is WorkflowBase, Ownable{
         for (uint256 i = 0; i < ids.length; i++) result[i] = items[ids[i]];
         return result;
 	}
+    /*
+        The mint function to create items that can then be rented out.
+     */
+	function registerItem(string calldata name,uint collateral,uint pricePerDay) external returns (uint256) {
+		uint256 id = _getNextId();
+		Rental memory item;
+		item.id = id;
+		items[id] = item;
+		_checkOwner();
+		item.name = name;
+		item.collateral = collateral;
+		item.pricePerDay = pricePerDay;
+		item.status = 3;
+		items[id] = item;
+		emit ItemUpdated(id, item.status);
+		return id;
+	}
+    /*
+        Update to change the item details and rent conditions before rent has started
+     */
+	function update(uint256 id,string calldata name,uint collateral,uint pricePerDay) external returns (uint256) {
+		Rental memory item = getItem(id);
+		_checkOwner();
+		_assertStatus(item, 3);
+		item.name = name;
+		item.collateral = collateral;
+		item.pricePerDay = pricePerDay;
+		item.status = 3;
+		items[id] = item;
+		emit ItemUpdated(id, item.status);
+		return id;
+	}
 	function startRent(uint256 id,uint64 numberOfDays,uint allowance) external returns (uint256) {
 		Rental memory item = getItem(id);
 		_assertOnlyRenter(item);
@@ -72,115 +113,11 @@ contract RentalWorkflow  is WorkflowBase, Ownable{
 		emit ItemUpdated(id, item.status);
 		return id;
 	}
-	function charge(uint256 id) external returns (uint256) {
-		Rental memory item = getItem(id);
-		_checkOwner();
-		_assertStatus(item, 1);
-		uint daysToCharge = ( ( block.timestamp - item.startTime ) / ( ( 24 * 60 ) * 60 ) ) - item.daysCharged;
-		item.daysCharged = item.daysCharged + daysToCharge;
-		item.leftoverCharge = daysToCharge * item.pricePerDay;
-		item.status = 1;
-		items[id] = item;
-		if (owner() != address(0) && item.renter != address(0) && item.leftoverCharge > 0){
-			safeTransferFromExternal(token, item.renter, owner(), item.leftoverCharge);
-		}
-		emit ItemUpdated(id, item.status);
-		return id;
-	}
-	function endRental(uint256 id) external returns (uint256) {
-		Rental memory item = getItem(id);
-		_checkOwner();
-		_assertStatus(item, 1);
-		uint nominalFee = ( item.numberOfDays - item.daysCharged ) * item.pricePerDay;
-		uint endTime = item.startTime + ( ( ( item.numberOfDays * 24 ) * 60 ) * 60 );
-		item.leftoverCharge = nominalFee + ( ( block.timestamp > endTime ) ? ( ( endTime - block.timestamp ) * ( ( ( item.pricePerDay / 24 ) / 60 ) / 60 ) ) : 0 );
-		item.status = 2;
-		items[id] = item;
-		emit ItemUpdated(id, item.status);
-		return id;
-	}
-	function registerItem(string calldata name,uint collateral,uint pricePerDay) external returns (uint256) {
-		uint256 id = _getNextId();
-		Rental memory item;
-		item.id = id;
-		items[id] = item;
-		_checkOwner();
-		item.name = name;
-		item.collateral = collateral;
-		item.pricePerDay = pricePerDay;
-		item.status = 3;
-		items[id] = item;
-		emit ItemUpdated(id, item.status);
-		return id;
-	}
-	function finalCharge(uint256 id,uint chargeAmount) external returns (uint256) {
-		Rental memory item = getItem(id);
-		_checkOwner();
-		_assertStatus(item, 2);
-		require(chargeAmount <= item.leftoverCharge, "Cannot charge more than leftover");
-
-		item.status = ( chargeAmount == item.leftoverCharge ) ? 4 : 2;
-		items[id] = item;
-		if (owner() != address(0) && item.renter != address(0) && chargeAmount > 0){
-			safeTransferFromExternal(token, item.renter, owner(), chargeAmount);
-		}
-		emit ItemUpdated(id, item.status);
-		return id;
-	}
-	function chargeWithCollateral(uint256 id) external returns (uint256) {
-		Rental memory item = getItem(id);
-		_checkOwner();
-		_assertStatus(item, 2);
-		require(item.collateral >= item.leftoverCharge, "Has collateral");
-		item.collateral = item.collateral - item.leftoverCharge;
-		item.status = 4;
-		items[id] = item;
-		emit ItemUpdated(id, item.status);
-		return id;
-	}
-	function releaseCollateral(uint256 id,uint chargeAmount,string calldata /*evidence*/) external returns (uint256) {
-		Rental memory item = getItem(id);
-		_assertStatus(item, 4);
-		require(item.collateral >= chargeAmount, "Has collateral?");
-		item.collateral = item.collateral - chargeAmount;
-		item.status = 5;
-		items[id] = item;
-		if (item.renter != address(0) && item.collateral > 0){
-			safeTransferExternal(token, item.renter, item.collateral);
-		}
-		emit ItemUpdated(id, item.status);
-		return id;
-	}
-	function makeAvailable(uint256 id,string calldata name,uint collateral,uint pricePerDay) external returns (uint256) {
-		Rental memory item = getItem(id);
-		_checkOwner();
-		_assertStatus(item, 5);
-		item.name = name;
-		item.collateral = collateral;
-		item.pricePerDay = pricePerDay;
-		item.renter = address(0);
-		item.startTime = 0;
-		item.leftoverCharge = 0;
-		item.daysCharged = 0;
-		item.numberOfDays = 0;
-		item.collateral = 0;
-		item.status = 3;
-		items[id] = item;
-		emit ItemUpdated(id, item.status);
-		return id;
-	}
-	function update(uint256 id,string calldata name,uint collateral,uint pricePerDay) external returns (uint256) {
-		Rental memory item = getItem(id);
-		_checkOwner();
-		_assertStatus(item, 3);
-		item.name = name;
-		item.collateral = collateral;
-		item.pricePerDay = pricePerDay;
-		item.status = 3;
-		items[id] = item;
-		emit ItemUpdated(id, item.status);
-		return id;
-	}
+    /*
+        Ends the rent, returns collateral (in full) and collects all outstanding charges.
+        The nominal fee is the contractual amount (days * fee). Even if rent is less than that, it will be charged.
+        If the rent is more than that, then every minute over will be charged extra (daily rent divided by minutes in the day)
+    */
 	function endAndSettle(uint256 id) external returns (uint256) {
 		Rental memory item = getItem(id);
 		_checkOwner();
@@ -199,12 +136,108 @@ contract RentalWorkflow  is WorkflowBase, Ownable{
 		emit ItemUpdated(id, item.status);
 		return id;
 	}
-	function updateDuration(uint256 id,uint64 numberOfDays) external returns (uint256) {
+    /*
+        The collect rent function. Useful in long-running rent contracts (e.g. monthly).
+        The outstanding rent payment is calculated (based on last charge time) deducted from allowance. 
+     */
+	function charge(uint256 id) external returns (uint256) {
 		Rental memory item = getItem(id);
 		_checkOwner();
 		_assertStatus(item, 1);
-		item.numberOfDays = numberOfDays;
+		uint daysToCharge = ( ( block.timestamp - item.startTime ) / ( ( 24 * 60 ) * 60 ) ) - item.daysCharged;
+		item.daysCharged = item.daysCharged + daysToCharge;
+		item.leftoverCharge = daysToCharge * item.pricePerDay;
 		item.status = 1;
+		items[id] = item;
+		if (owner() != address(0) && item.renter != address(0) && item.leftoverCharge > 0){
+			safeTransferFromExternal(token, item.renter, owner(), item.leftoverCharge);
+		}
+		emit ItemUpdated(id, item.status);
+		return id;
+	}
+    /*
+        In case of disagreements or if the allowance is not enough the rent can be still factually ended without payments and moved to an intermediate 'returned' state.
+     */
+	function endRental(uint256 id) external returns (uint256) {
+		Rental memory item = getItem(id);
+		_checkOwner();
+		_assertStatus(item, 1);
+		uint nominalFee = ( item.numberOfDays - item.daysCharged ) * item.pricePerDay;
+		uint endTime = item.startTime + ( ( ( item.numberOfDays * 24 ) * 60 ) * 60 );
+		item.leftoverCharge = nominalFee + ( ( block.timestamp > endTime ) ? ( ( endTime - block.timestamp ) * ( ( ( item.pricePerDay / 24 ) / 60 ) / 60 ) ) : 0 );
+		item.status = 2;
+		items[id] = item;
+		emit ItemUpdated(id, item.status);
+		return id;
+	}
+    /*
+        Charge from allowance is done in the amount of the outstanding fee.
+     */
+	function finalCharge(uint256 id,uint chargeAmount) external returns (uint256) {
+		Rental memory item = getItem(id);
+		_checkOwner();
+		_assertStatus(item, 2);
+		require(chargeAmount <= item.leftoverCharge, "Cannot charge more than leftover");
+
+		item.status = ( chargeAmount == item.leftoverCharge ) ? 4 : 2;
+		items[id] = item;
+		if (owner() != address(0) && item.renter != address(0) && chargeAmount > 0){
+			safeTransferFromExternal(token, item.renter, owner(), chargeAmount);
+		}
+		emit ItemUpdated(id, item.status);
+		return id;
+	}
+    /*
+        In case allowance is not increasing for whatever reason (lost wallet, bad actor)
+        then it is possible to deduct leftover charge from collateral.
+     */
+	function chargeWithCollateral(uint256 id) external returns (uint256) {
+		Rental memory item = getItem(id);
+		_checkOwner();
+		_assertStatus(item, 2);
+		require(item.collateral >= item.leftoverCharge, "Has collateral");
+		item.collateral = item.collateral - item.leftoverCharge;
+		item.status = 4;
+		items[id] = item;
+		emit ItemUpdated(id, item.status);
+		return id;
+	}
+
+    /*
+        To release collateral from the smart contract. The owner can specify charge amount if there is potential damage to the item.
+        For example if collateral is 1000 and damage is 200, then renter would receive 800 and owner 200.
+    */
+	function releaseCollateral(uint256 id,uint chargeAmount,string calldata /*evidence*/) external returns (uint256) {
+		Rental memory item = getItem(id);
+		_assertStatus(item, 4);
+		require(item.collateral >= chargeAmount, "Has collateral?");
+		item.collateral = item.collateral - chargeAmount;
+		item.status = 5;
+		items[id] = item;
+		if (item.renter != address(0) && item.collateral > 0){
+			safeTransferExternal(token, item.renter, item.collateral);
+		}
+		emit ItemUpdated(id, item.status);
+		return id;
+	}
+    /*
+        In case the same item can go back to circulation, this provides a convenience method.
+        The benefit is that the item retains full history of all rentals.
+     */
+	function makeAvailable(uint256 id,string calldata name,uint collateral,uint pricePerDay) external returns (uint256) {
+		Rental memory item = getItem(id);
+		_checkOwner();
+		_assertStatus(item, 5);
+		item.name = name;
+		item.collateral = collateral;
+		item.pricePerDay = pricePerDay;
+		item.renter = address(0);
+		item.startTime = 0;
+		item.leftoverCharge = 0;
+		item.daysCharged = 0;
+		item.numberOfDays = 0;
+		item.collateral = 0;
+		item.status = 3;
 		items[id] = item;
 		emit ItemUpdated(id, item.status);
 		return id;
