cdb28661498b7d50bff4e0b2c799ea2abbda2dec
sgraphics <silver.sepp@gmail.com>
2023-08-25 12:33:49 +0300

    Minor whitespace correction

--- Contracts/WorkflowBase.sol
+++ Contracts/WorkflowBase.sol
@@ -3,40 +3,40 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 contract WorkflowBase is Context {
-    uint256 internal count = 0;
+	uint256 internal count = 0;
 	IExternalServiceLocator internal serviceLocator;
-    function _getNextId() internal returns (uint256) {
-        count++;
-        return count;
-    }
-    function getLatestIds(uint256 cnt) public view returns(uint256[] memory) {
-        uint256 toIndex = count;
-        uint256 fromIndex = 0;
-        if (cnt < toIndex) {
-            fromIndex = toIndex - cnt;
-        }
-        if (fromIndex > toIndex || toIndex < 0) {
-            return new uint256[](0);
-        }
-        uint256[] memory latestIds = new uint256[](cnt);
-        uint256 setterCount = 0;
-        for(uint256 i=fromIndex; i < toIndex; i++) {
-            latestIds[setterCount] = i + 1;
-            setterCount++;
-        }
-        return latestIds;
-    }
-    function getPageIds(uint256 cursor, uint256 howMany) public view returns(uint256[] memory) {
-        uint256[] memory idsToReturn = new uint256[](howMany);
-        uint256 len = 0;
-        while (cursor <= count && len < howMany) {
-            idsToReturn[len] = cursor;
-            len++;
-            cursor++;
-        }
-        return idsToReturn;
-    }    
-    function addFkMappingItem(mapping(uint => uint[]) storage itemMap, uint foreignKey, uint itemId) internal {
+	function _getNextId() internal returns (uint256) {
+		count++;
+		return count;
+	}
+	function getLatestIds(uint256 cnt) public view returns(uint256[] memory) {
+		uint256 toIndex = count;
+		uint256 fromIndex = 0;
+		if (cnt < toIndex) {
+			fromIndex = toIndex - cnt;
+		}
+		if (fromIndex > toIndex || toIndex < 0) {
+			return new uint256[](0);
+		}
+		uint256[] memory latestIds = new uint256[](cnt);
+		uint256 setterCount = 0;
+		for(uint256 i=fromIndex; i < toIndex; i++) {
+			latestIds[setterCount] = i + 1;
+			setterCount++;
+		}
+		return latestIds;
+	}
+	function getPageIds(uint256 cursor, uint256 howMany) public view returns(uint256[] memory) {
+		uint256[] memory idsToReturn = new uint256[](howMany);
+		uint256 len = 0;
+		while (cursor <= count && len < howMany) {
+			idsToReturn[len] = cursor;
+			len++;
+			cursor++;
+		}
+		return idsToReturn;
+	}	
+	function addFkMappingItem(mapping(uint => uint[]) storage itemMap, uint foreignKey, uint itemId) internal {
 		itemMap[foreignKey].push(itemId);
 	}
 	function removeFkMappingItem(mapping(uint => uint[]) storage itemMap, uint foreignKey, uint itemId) internal {
@@ -57,10 +57,10 @@ contract WorkflowBase is Context {
 			itemArray.pop();
 		}
 	}
-    function toString(uint256 value) internal pure returns (string memory) {
+	function toString(uint256 value) internal pure returns (string memory) {
 		return Strings.toString(value);
-	}    
-    function safeTransferFromExternal(address token_, address from, address to, uint value) internal {
+	}	
+	function safeTransferFromExternal(address token_, address from, address to, uint value) internal {
 		(bool success, bytes memory data) = token_.call(abi.encodeWithSelector(0x23b872dd, from, to, value));
 		require(success && (data.length == 0 || abi.decode(data, (bool))), 'TransferHelper::transferFrom: transferFrom failed');
 	}
