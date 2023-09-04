3ad2c9bbc52205b7e60360c8fd229f76792b8dc4
sgraphics <silver.sepp@gmail.com>
2023-09-04 13:34:49 +0300

    Update fix non transferrable missing virtual keyword

--- Contracts/NonTransferrableERC721.sol
+++ Contracts/NonTransferrableERC721.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NonTransferrableERC721 is ERC721 {    
     constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {}
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize) internal override(ERC721)
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize) internal virtual override(ERC721)
     {
         require(from == address(0), "Token not transferable");
         super._beforeTokenTransfer(from, to, tokenId, batchSize);
