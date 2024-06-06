90750b3e56041a4d8875e6eb7c8308e8d3f48920
Silver Sepp <silver.sepp@gmail.com>
2024-06-06 15:53:07 +0300

    remove unnecessary base classes

--- Contracts/WorkflowBase.sol
+++ Contracts/WorkflowBase.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 import "./WorkflowBaseCommon.sol";
-import "@openzeppelin/contracts/utils/Context.sol";
-contract WorkflowBase is WorkflowBaseCommon, Context {
+contract WorkflowBase is WorkflowBaseCommon  {
 }
--- Contracts/WorkflowBaseUpgradeable.sol
+++ Contracts/WorkflowBaseUpgradeable.sol
@@ -1,11 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 import "./WorkflowBaseCommon.sol";
-import "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract WorkflowBaseUpgradeable is Initializable, WorkflowBaseCommon, ContextUpgradeable {
-    function initialize() public initializer {
-        __Context_init();
-    }
+contract WorkflowBaseUpgradeable is WorkflowBaseCommon {
 }
