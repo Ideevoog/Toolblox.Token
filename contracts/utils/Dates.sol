4f78edca97ec69ea7ebca44a18956c26bbcd8c84
Silver Sepp <silver.sepp@gmail.com>
2024-10-12 22:33:58 +0300

    Add dates methods

--- contracts/utils/Dates.sol
+++ contracts/utils/Dates.sol
@@ -6,8 +6,20 @@ pragma solidity ^0.8.20;
  * @dev Date operations.
  */
 library Dates {
-    uint256 constant SECONDS_PER_DAY = 86400;
+    /**
+    * @dev Normalizes a timestamp to midnight (00:00:00) of the same date.
+    * @param timestamp The original timestamp.
+    * @return The normalized timestamp at midnight.
+    */
     function getDate(uint256 timestamp) public pure returns (uint256) {
-        return (timestamp / SECONDS_PER_DAY) * SECONDS_PER_DAY;
+        return (timestamp / 1 days) * 1 days; 
+    }
+    /**
+     * @dev Converts a number of days into seconds.
+     * @param numberOfDays The number of days.
+     * @return The equivalent number of seconds.
+     */
+    function daysToSeconds(uint256 numberOfDays) public pure returns (uint256) {
+        return numberOfDays * 1 days;
     }
 }
